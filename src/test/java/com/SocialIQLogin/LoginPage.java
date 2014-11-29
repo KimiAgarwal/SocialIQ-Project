ed74bea9572d07a3ec32ab1c4c50b830a480de2b
kimi agarwal <kimi.agarwal@intelligrape.com>
2014-11-29 14:56:37 +0530

    create an email id with null value

---
+++ src/test/java/com/SocialIQLogin/LoginPage.java
@@ -0,0 +1,16 @@
+package com.SocialIQLogin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+	@Test
+	public void FirstNameValidationTest1() throws InterruptedException{
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.get("app.siq.qa2.intelligrape.net");
+	
+}
+}
